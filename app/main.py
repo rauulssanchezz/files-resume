77e98c510030cffd9e2b878c576b01b8b59d4afb
Raul Sanchez Sanchez <rauulssanchezz@gmail.com>
2026-04-23 11:30:33 +0200

    endpoint para obtener el file

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, HTTPException, status
 
 app = FastAPI(
     title="Files Resume Api"
 )
 
-@app.get("/")
-def hello_world():
-    return "Hola mundo"
+@app.post("/resume-file")
+async def resume_file(file: UploadFile):
+   if file.content_type != "text/plain":
+       raise HTTPException(
+           status_code=status.HTTP_400_BAD_REQUEST,
+           detail="El archivo solo puede conter texto"
+        )
+   return await file.read()
