bd47f5f0cf5404e8f45c3f4f5866b6311b46a602
Raul Sanchez Sanchez <rauulssanchezz@gmail.com>
2026-04-23 11:43:15 +0200

    implementacion de groq

--- app/main.py
+++ app/main.py
@@ -1,16 +1,41 @@
 from fastapi import FastAPI, UploadFile, HTTPException, status
 import os
 from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
 
 app = FastAPI(
-    title="Files Resume Api"
+    title="Files Resumer Api"
 )
 
 @app.post("/resume-file")
 async def resume_file(file: UploadFile):
-   if file.content_type != "text/plain":
-       raise HTTPException(
-           status_code=status.HTTP_400_BAD_REQUEST,
-           detail="El archivo solo puede conter texto"
+    if file.content_type != "text/plain":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="El archivo solo puede conter texto"
         )
-   return await file.read()
+
+    file_content = await file.read()
+   
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "name": "File Resumer",
+                "content": "Te encargas de resumir el contenido de los archivos localizando los puntos mas importantes."
+            },
+            {
+                "role": "user",
+                "content": f"Resume este texto: {file_content} ",
+            }
+        ],
+        model="llama-3.3-70b-versatile",
+    )
+
+    return chat_completion.choices[0].message.content
